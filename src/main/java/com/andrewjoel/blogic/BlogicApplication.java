d4678ad9a8164ad6b9e7f79212fd0b00f3628d1c
ajoel24 <drew.devops@gmail.com>
2022-06-04 07:04:09 +0530

    Initial commit

---
+++ src/main/java/com/andrewjoel/blogic/BlogicApplication.java
@@ -0,0 +1,13 @@
+package com.andrewjoel.blogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogicApplication.class, args);
+	}
+
+}
